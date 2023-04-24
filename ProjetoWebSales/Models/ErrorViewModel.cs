791c43743ef38eed5e910851fe93c742db4d1a19
Andreza Abrantes <andrezaabrantes@gmail.com>
2023-04-23 21:10:51 -0300

    First commit

---
+++ ProjetoWebSales/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjetoWebSales.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
