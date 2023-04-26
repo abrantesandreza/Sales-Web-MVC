7ade9cbe8b5e364520bf5529d18231b2a9206510
Andreza Abrantes <andrezadossantosabrantes@gmail.com>
2023-04-26 15:47:44 -0300

    Simple Create Form

--- ProjetoWebSales/Controllers/SellersController.cs
+++ ProjetoWebSales/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoWebSales.Models;
 using ProjetoWebSales.Services;
 
 namespace ProjetoWebSales.Controllers
@@ -16,5 +17,19 @@ namespace ProjetoWebSales.Controllers
             var list = _sellerService.FindAll();
             return View(list);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken] //Previne ataques CSRF
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index)); //melhor manutenabilidade
+        }
+
     }
 }
--- ProjetoWebSales/Services/SellerService.cs
+++ ProjetoWebSales/Services/SellerService.cs
@@ -16,5 +16,11 @@ namespace ProjetoWebSales.Services
         {
             return _context.Seller.ToList();
         }
+
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
     }
 }
