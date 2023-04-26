2d776c4f9eb18d138cb74dc064667b0bcb7b8a0f
Andreza Abrantes <andrezadossantosabrantes@gmail.com>
2023-04-26 16:08:20 -0300

    Adding Constraint-DepartmentId

--- ProjetoWebSales/Services/SellerService.cs
+++ ProjetoWebSales/Services/SellerService.cs
@@ -19,6 +19,7 @@ namespace ProjetoWebSales.Services
 
         public void Insert(Seller obj)
         {
+            obj.Department = _context.Department.First();
             _context.Add(obj);
             _context.SaveChanges();
         }
